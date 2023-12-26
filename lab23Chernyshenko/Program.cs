191dafbe5e2462f019c43eee62d9129a69ac12cd
uliyana <uliyaiyaas@gmail.com>
2023-12-26 12:33:52 +0200

    Добавьте файлы проекта.

---
+++ lab23Chernyshenko/Program.cs
@@ -0,0 +1,29 @@
+﻿class Polusharie
+{
+    static void Main()
+    {
+        CheckHemisphere(30, 121);
+        CheckHemisphere(-15, 50);
+        CheckHemisphere(87, 223);
+    }
+    static void CheckHemisphere(int F, int D)
+    {
+        if (F >= 0)
+        {
+            Console.Write("Северo - ");
+        }
+        else
+        {
+            Console.Write("Южно - ");
+        }
+
+        if (D >= 0)
+        {
+            Console.WriteLine("восточное полушарие.");
+        }
+        else
+        {
+            Console.WriteLine("западное полушарие.");
+        }
+    }
+}
